1ea670143c14a3136aa36ad5f45a3516b70940d8
tsmith <tsmith@mackays.co.uk>
2016-11-11 16:29:51 +0000

    Updated server file to handle messages a little better

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ var dateFormat = require('dateformat');
 var ws = require("nodejs-websocket")
 var watchPath = ('.\\TestWatch\\');
 
-
 var watchObject = {};
 watchObject.path = watchPath;
 watchObject.name = "Test1";
+
 
 
 var server = ws.createServer(function (conn) {
@@ -53,34 +53,19 @@ var watcher = chokidar.watch(watchPath, {
   atomic: true // or a custom 'atomicity delay', in milliseconds (default 100)
 });
 
-
-
-
-
-
-
 watcher
-  .on('add', (filename, details) => handleCase(filename, timeNow(), 'add'))
+  .on('add', filename => handleCase(filename, timeNow(), 'add'))
   .on('change', (filename, details) => handleCase(filename, timeNow(), 'mod'))
-  .on('unlink',  (filename) => handleCase(filename, timeNow(), 'del'));
-
-watcher
-  .on('addDir', path => broadcast(server, `Directory ${path} has been added`))
-  .on('unlinkDir', path => log(`Directory ${path} has been removed`))
-  .on('error', error => log(`Watcher error: ${error}`));
-
-//  .on('raw', (event, path, details) => {
-//    log('Raw event info:', event, path, details
+  .on('unlink',  filename => handleCase(filename, timeNow(), 'del'));
   
-// 'add', 'addDir' and 'change' events also receive stat() results as second
-// argument when available: http://nodejs.org/api/fs.html#fs_class_fs_stats
-watcher.on('change', (path, stats) => {
-  if (stats) console.log(`File ${path} changed size to ${stats.mtime}`);
-});
+watcher
+  .on('addDir', path => handleCase(path, timeNow(), 'add'))
+  .on('unlinkDir', path => handleCase(path, timeNow(), 'del'))
+  .on('error', error => errorHandler(error, timeNow()));
 
-//  .on('add', (filename, details) => broadcast(server, `{ \"FileName\":\"${filename}\", \"TimeStamp\":\"${details.mtime}\" }`))
-
-function happyPath(path)     
+var watchedPaths = watcher.getWatched();
+  
+function happyPath(path)
 {     
     return path.replace(/\\/g, "\\\\");
 } 
@@ -88,6 +73,25 @@ function happyPath(path)
 function timeNow() {
 	var now = new Date();
 	return dateFormat(now,"HH:MM:ss.l   (dd-mm-yyyy)");
+}
+
+function errorHandler(error, timeStamp)
+{
+	var message = [];
+	message.push('{ ');
+	
+	message.push(`\"Error\":`);
+	message.push(`\"` + error + `\"`);
+	message.push(`,`);
+	
+	message.push(`\"TimeStamp\":`);
+	message.push(`\"` + timeStamp + `\"`);
+		
+	message.push(' }');
+	
+	console.log(message.join(""));
+		
+	broadcast(server, message.join(""));
 }
 
 function handleCase(fileName, timeStamp, eventType)
@@ -117,12 +121,9 @@ function handleCase(fileName, timeStamp, eventType)
 	broadcast(server, message.join(""));
 }
 
-
 function broadcast(server, msg) {
     server.connections.forEach(function (conn) {
         conn.sendText(msg)
     })
 }
-
-var watchedPaths = watcher.getWatched();
 
